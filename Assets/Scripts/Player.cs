a6f4eb7aebf393378ee114504554e51e8726ddb4
Andrea Alicino <andrea.alicino@gmail.com>
2021-07-23 18:37:50 +0200

    player first movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    [SerializeField]
+    private float _speed;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        _rb.velocity = new Vector2(horizontalInput /** _speed * Time.deltaTime*/,_rb.velocity.y);
+        //using velocity 
+        //add rigidbody 
+        //collider 
+    }
+}
