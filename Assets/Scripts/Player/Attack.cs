c6023a191e55870f0130cf66f3db371655ae522b
Andrea Alicino <andrea.alicino@gmail.com>
2021-08-04 16:34:00 +0200

    sword hit box and enemy collision detection

---
+++ Assets/Scripts/Player/Attack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log(other.name);
+        }
+
+        
+    }
+}
